fccfbd1afef5e9ad088d689d8a8df92f84457145
boboboba <trascinsavelij@gmail.com>
2025-10-28 20:49:44 +0500

    fix

--- sql/drop.sql
+++
@@ -1,8 +0,0 @@
-DO $$ DECLARE
-    r RECORD;
-BEGIN
-    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
-        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-        RAISE NOTICE 'Удалена таблица: %', r.tablename;
-    END LOOP;
-END $$;
