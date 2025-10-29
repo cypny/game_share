9eb73bf65d1f20e44d7db40811c7af0afb2e64da
boboboba <trascinsavelij@gmail.com>
2025-10-29 23:18:49 +0500

    feat: simple game search

--- game_share_bot/core/callbacks/games/catalog.py
+++ game_share_bot/core/callbacks/games/catalog.py
@@ -2,4 +2,5 @@ from aiogram.filters.callback_data import CallbackData
 
 
 class CatalogCallback(CallbackData, prefix="catalog"):
+    query: str = ""
     page: int = 0
--- game_share_bot/core/handlers/games/catalog.py
+++ game_share_bot/core/handlers/games/catalog.py
@@ -1,5 +1,5 @@
 from aiogram import Router, types
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, Message, InlineKeyboardMarkup
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from game_share_bot.core.callbacks import CatalogCallback, MenuCallback
@@ -15,53 +15,51 @@ logger = get_logger(__name__)
 
 @router.callback_query(CatalogCallback.filter())
 async def catalog(callback: CallbackQuery, callback_data: CatalogCallback, session: AsyncSession):
-    user_id = callback.from_user.id
-    logger.info(f"Пользователь {user_id} открыл каталог")
-
     try:
-        game_repo = GameRepository(session)
+        query = callback_data.query
         page = callback_data.page
-        page_size = 5
-
-        games = await game_repo.get_all(
-            skip=page * page_size,
-            take=page_size
-        )
-
-        total_games = await game_repo.count_all()
-
-        logger.debug(f"Получено {len(games)} игр для пользователя {user_id}")
-
-        games_str = format_games_list(games)
-        reply = f"Каталог игр:\n\n{games_str}"
+        reply, reply_markup = await _process_search_game(query, page, session)
 
         await callback.answer()
         await callback.message.edit_text(
             reply,
             parse_mode="HTML",
-            reply_markup=catalog_keyboard(page, total_games, page_size)
+            reply_markup=reply_markup
         )
-        logger.info(f"Каталог успешно отправлен пользователю {user_id}")
 
     except Exception as e:
-        logger.error(f"Ошибка при получении каталога для пользователя {user_id}: {str(e)}", exc_info=True)
         await callback.answer("❌ Ошибка при загрузке каталога")
 
-# @router.message()
-# async def search_game(message: types.Message, session: AsyncSession):
-#     game_repo = GameRepository(session)
-#
-#     text = message.text
-#     for game in await game_repo.get_all():
-#         if text.lower() in game.title.lower():
-#             reply = format_game_full(game)
-#
-#             await message.answer_photo(
-#                 caption=reply,
-#                 parse_mode="HTML",
-#                 photo=game.cover_image_url
-#             )
-#             return
-#     await message.answer("Не найдено")
+@router.message(lambda message: not message.text.startswith('/'))
+async def search_game(message: types.Message, session: AsyncSession):
+    query = message.text
+    page = 0
+    reply, reply_markup = await _process_search_game(query, page, session)
+    await message.answer(
+        reply,
+        parse_mode="HTML",
+        reply_markup=reply_markup
+    )
+
+async def _process_search_game(query: str, page:int, session: AsyncSession) -> tuple[str, InlineKeyboardMarkup]:
+    game_repo = GameRepository(session)
+    page_size = 2
+    if not query:
+        games = await game_repo.get_all(
+            skip=page * page_size,
+            take=page_size
+        )
+        total_games = await game_repo.count_all()
+    else:
+        games, total_games = await game_repo.search_games(
+            query,
+            skip=page * page_size,
+            take=page_size
+        )
+
+    games_str = format_games_list(games)
+    reply = f"Каталог игр:\n\n{games_str}"
+
+    return reply, catalog_keyboard(page, total_games, page_size, query)
 
 
