5b41e7b1b7c9072e2bb0e7160015a12f529fe952
Aleksey <skvch.al@gmail.com>
2025-11-20 12:11:44 +0500

    feat: add to_main_menu button

--- game_share_bot/core/keyboards/inline/buttons.py
+++ game_share_bot/core/keyboards/inline/buttons.py
@@ -1,6 +1,13 @@
 from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardButton
 
+from game_share_bot.core.callbacks import MenuCallback
+from game_share_bot.domain.enums import MenuSection
+
 
 def return_button(callback: CallbackData) -> InlineKeyboardButton:
     return InlineKeyboardButton(text="⬅️ Назад", callback_data=callback.pack())
+
+
+def to_main_menu_button() -> InlineKeyboardButton:
+    return InlineKeyboardButton(text="⬅️ В главное меню", callback_data=MenuCallback(section=MenuSection.MAIN).pack())
