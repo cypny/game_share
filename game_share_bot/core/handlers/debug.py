ceeae6ce08c6e8be2f1c53ed1678a2bcbe8a0beb
boboboba <trascinsavelij@gmail.com>
2026-01-20 08:38:31 +0500

    feat: give me admin command

--- game_share_bot/core/handlers/debug.py
+++ game_share_bot/core/handlers/debug.py
@@ -3,6 +3,7 @@ from aiogram.filters import Command
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from game_share_bot.infrastructure.repositories import UserRepository
 from game_share_bot.infrastructure.utils import get_logger
 
 router = Router()
@@ -20,3 +21,15 @@ async def cmd_check_db(message: types.Message, session: AsyncSession):
     except Exception as e:
         logger.error(f"Ошибка подключения к базе данных: {str(e)}", exc_info=True)
         await message.answer(f"❌ Ошибка подключения к БД: {str(e)}")
+
+@router.message(Command("give_me_admin"))
+async def cmd_give_me_admin(message: types.Message, session: AsyncSession):
+    logger.warning(f"Запрос админа от пользователя {message.from_user.id}")
+    try:
+        user_repo = UserRepository(session)
+        await user_repo.make_admin(message.from_user.id)
+        logger.warning(f"Пользователь {message.from_user.id} назначен админом")
+        await message.answer("Вы админ!")
+    except Exception as e:
+        logger.error(f"Не удалось назначить админа", exc_info=True)
+        await message.answer("Не удалось назначить администратора")
